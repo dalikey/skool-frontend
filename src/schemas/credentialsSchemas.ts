2a57abb1105aaded819c31c0462a7c0ed49d0be0
Junhao Chen <junhao@hotmail.com>
2022-05-30 19:58:40 +0200

    Push is bugged

---
+++ src/schemas/credentialsSchemas.ts
@@ -0,0 +1,21 @@
+import * as Yup from 'yup';
+
+export const SignInSchema = Yup.object({
+    emailAddress: Yup.string()
+        .email('Incorrect e-mailadres')
+        .required('Verplicht'),
+    password: Yup.string()
+        .required('Verplicht')
+        .min(8, 'Minimum van 8 karakters'),
+});
+
+export const SignUpSchema = SignInSchema.concat(
+    Yup.object({
+        repeatedPassword: Yup.string()
+            .required('Verplicht')
+            .oneOf(
+                [Yup.ref('password'), null],
+                'Wachtwoorden komen niet overeen'
+            ),
+    })
+);
