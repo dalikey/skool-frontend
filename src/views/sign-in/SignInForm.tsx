5ab335802e565f44f9fad477767a5e2fca12fe0f
Rick van Loon <rick@bumbal.eu>
2022-06-01 13:18:54 +0200

    Added error message

--- src/views/sign-in/SignInForm.tsx
+++ src/views/sign-in/SignInForm.tsx
@@ -1,13 +1,13 @@
-import { Button, Stack, TextField, Link } from '@mui/material';
+import { Button, Stack, TextField, Link, FormHelperText } from '@mui/material';
 import { useFormik } from 'formik';
 import { CredentialsModel } from '../../models/authModels';
 import { useLoginMutation } from '../../api/auth/authApi';
 
 const SignInForm = () => {
-    const [register] = useLoginMutation();
+    const [login, { isError }] = useLoginMutation();
 
     const handleSignIn = (values: CredentialsModel) => {
-        register(values);
+        login(values);
     };
 
     const formik = useFormik({
@@ -21,10 +21,15 @@ const SignInForm = () => {
     return (
         <form onSubmit={formik.handleSubmit}>
             <Stack spacing={2}>
+                {isError && (
+                    <FormHelperText error={true}>
+                        E-mailadres en wachtwoord komen niet overeen
+                    </FormHelperText>
+                )}
                 <TextField
                     id='emailAddress'
                     name='emailAddress'
-                    label='Email adres'
+                    label='E-mailadres'
                     value={formik.values.emailAddress}
                     onChange={formik.handleChange}
                     variant='standard'
