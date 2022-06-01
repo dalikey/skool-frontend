87980d502820182c7a2955638129fd5fc14a74bf
rickvloon <mail@rickvanloon.nl>
2022-06-01 23:41:11 +0200

    Protected applayout route

--- src/app/useLocalStorage.ts
+++ src/app/useLocalStorage.ts
@@ -1,21 +1,21 @@
 import { useState, useEffect } from 'react';
 
 function getSavedValue<T>(key: string, initialValue: T): T {
-    const savedValue = JSON.parse(localStorage.getItem(key) ?? 'undefined');
+    const savedValue = localStorage.getItem(key);
 
-    if (savedValue) return savedValue;
+    if (savedValue) return JSON.parse(savedValue);
 
     if (initialValue instanceof Function) return initialValue;
 
     return initialValue;
 }
 
-export function useLocalStorage<T>(key: string, initialValue: T) {
+export function useLocalStorage<T>(key: string, initialValue: T): [T, Function] {
     const [value, setValue] = useState<T>(() => getSavedValue(key, initialValue));
 
     useEffect(() => {
         localStorage.setItem(key, JSON.stringify(value));
-    }, [value]);
+    }, [key, value]);
 
     return [value, setValue];
 }
