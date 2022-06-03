86a188e8bbba4e83e71053700948742881a065b3
rickvloon <mail@rickvanloon.nl>
2022-06-03 13:09:14 +0200

    BASE_URL based on env variable

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const BASE_URL =
     process.env.NODE_ENV === 'production'
-        ? 'https://skool-stable.herokuapp.com/api'
+        ? process.env.BASE_URL
         : 'https://skool-development.herokuapp.com/api';
 
 export const api = createApi({
