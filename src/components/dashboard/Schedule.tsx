6f69c6d12a20aa975a2ad225ec16e0c6a09c6168
jeffrey <jeffreyvantillo@gmail.com>
2022-06-05 01:59:52 +0200

    added schedule component

---
+++ src/components/dashboard/Schedule.tsx
@@ -0,0 +1,13 @@
+const Schedule = () => {
+       
+    return (
+        <>
+            dit is een datepicker
+
+            dit is een rooster van de week
+
+        </>
+    );
+};
+
+export default Schedule;
