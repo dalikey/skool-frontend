098f742eafed68f3982ea540a6fe428d5b9064a4
rickvloon <mail@rickvanloon.nl>
2022-06-04 14:02:25 +0200

    ENV variable naming fix

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const BASE_URL =
     process.env.NODE_ENV === 'production'
-        ? process.env.BASE_URL
+        ? process.env.REACT_APP_BASE_URL
         : 'https://skool-development.herokuapp.com/api';
 
 export const api = createApi({
