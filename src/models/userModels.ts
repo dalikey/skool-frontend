6d4e3ba8c529f2186740e6c4a81829a011b999f5
rickvloon <mail@rickvanloon.nl>
2022-06-05 20:15:06 +0200

    Created usermodel

--- src/models/userModels.ts
+++ src/models/userModels.ts
@@ -5,3 +5,12 @@ export interface RegistrationModel {
     lastName: string;
     emailAddress: string;
 }
+
+export interface UserModel {
+    _id: string;
+    firstName: string;
+    lastName: string;
+    emailAddress: string;
+    role: 'owner' | 'user';
+    isActive: boolean | null;
+}
