65302cf72bfb531abc0e26a00bd5cc1b9fc25460
rickvloon <mail@rickvanloon.nl>
2022-06-05 17:53:58 +0200

    Approute fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { theme } from './app/theme';
 import AppLayout from './components/layout/AppLayout';
 import SignUp from './views/sign-up/SignUp';
 import SignIn from './views/sign-in/SignIn';
-import Board from './views/dashboard/board';
 import RegistrationManagement from './views/user-management/RegistrationManagement';
 
 const App = () => {
@@ -15,10 +14,10 @@ const App = () => {
                 <Route path='sign-up' element={<SignUp />} />
                 <Route path='sign-in' element={<SignIn />} />
                 <Route path='' element={<AppLayout />}>
-                    <Route path='dashboard' element={<Board />} />
-                    <Route path='registraties' element={<RegistrationManagement />} />
-                    <Route path='workshops' element={<div>workshops</div>} />
-                    <Route path='profiel' element={<div>profiel</div>} />
+                    <Route path='dashboard' element={<div></div>} />
+                    <Route path='gebruikersbeheer' element={<RegistrationManagement />} />
+                    <Route path='workshops' element={<div></div>} />
+                    <Route path='profiel' element={<div></div>} />
                 </Route>
             </Routes>
         </ThemeProvider>
