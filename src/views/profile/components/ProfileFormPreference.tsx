a0c0451398ebf8db38162b853fbbfc22c3f479cf
rickvloon <mail@rickvanloon.nl>
2022-06-09 12:13:57 +0200

    Added profile update form

---
+++ src/views/profile/components/ProfileFormPreference.tsx
@@ -0,0 +1,15 @@
+import { Grid } from '@mui/material';
+
+interface ProfileFormProps {
+    formik: any;
+}
+
+const ProfileFormPreference = ({ formik }) => {
+    return (
+        <Grid container columnSpacing={2} rowSpacing={2}>
+
+        </Grid>
+    );
+};
+
+export default ProfileFormPreference;
