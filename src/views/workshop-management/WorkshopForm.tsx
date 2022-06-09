889ea2dba2126b8a8cceda82a2b77ad491309de2
rickvloon <mail@rickvanloon.nl>
2022-06-09 12:58:56 +0200

    Added workshop form pop up

---
+++ src/views/workshop-management/WorkshopForm.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const WorkshopForm = () => {
+    return (
+        <div>tiddies</div>
+    );
+};
+
+export default WorkshopForm;
--- src/views/workshop-management/WorkshopOverview.tsx
+++ src/views/workshop-management/WorkshopOverview.tsx
@@ -1,11 +1,18 @@
-import { Box, Paper, Tab, Tabs } from '@mui/material';
+import { Paper } from '@mui/material';
 import ConfirmDialog from '../../components/dialog/ConfirmDialog';
+import FormDialog, { formDialog } from '../../components/dialog/FormDialog';
+import WorkshopForm from './WorkshopForm';
 
 const WorkshopOverview = () => {
+    const openWorkshopForm = () => {
+        console.log('cunt')
+        formDialog('Profiel bewerken', <WorkshopForm />);
+    };
+
     return (
         <Paper sx={{ width: '100%' }}>
-            <ConfirmDialog />
-            <button>Add</button>
+            <FormDialog />
+            <button onClick={openWorkshopForm}>Add</button>
             {/* <WorkshopTable  /> */}
         </Paper>
     );
