37b14ef45504f953e5dd212ecf10626986d4ed6e
sanneve <s.vaneck2@student.avans.nl>
2022-06-16 11:52:55 +0200

    finished 179

--- src/views/profile/Profile.tsx
+++ src/views/profile/Profile.tsx
@@ -1,20 +1,17 @@
 import {
     Grid,
     Typography,
-    IconButton,
     Avatar,
     Box,
     Button,
-} from '@mui/material';
-import {
-    Edit,
-    AttachFile as AttachFileIcon,
-} from '@mui/icons-material';
-import { useGetPersonalProfileQuery } from '../../api/user/userApi';
-import FormDialog, { formDialog } from '../../components/dialog/FormDialog';
-import ProfileForm from './ProfileForm';
-import ProfilePicture from '../../assets/no_profile_picture.jpg';
-import { useGetAllWorkshopsQuery } from '../../api/workshop/workshopApi';
+    Checkbox,
+} from "@mui/material";
+import { Edit } from "@mui/icons-material";
+import { useGetPersonalProfileQuery } from "../../api/user/userApi";
+import FormDialog, { formDialog } from "../../components/dialog/FormDialog";
+import ProfileForm from "./ProfileForm";
+import ProfilePicture from "../../assets/no_profile_picture.jpg";
+import { useGetAllWorkshopsQuery } from "../../api/workshop/workshopApi";
 
 const Profile = () => {
     const { data } = useGetPersonalProfileQuery();
@@ -23,142 +20,175 @@ const Profile = () => {
     let workshopString, workshopPref, levelString, levelPref;
     if (user?.workshopPreferences) {
         workshopString = user?.workshopPreferences;
-        workshopPref = workshopString.join('\r\n');
+        workshopPref = workshopString.join("\r\n");
     }
     if (user?.levelPreferences) {
         levelString = user?.levelPreferences;
-        levelPref = levelString.join('\r\n');
+        levelPref = levelString.join("\r\n");
     }
 
     const openProfileForm = () => {
         if (data?.result) {
-            formDialog('Profiel bewerken', <ProfileForm user={data.result} />);
+            formDialog("Profiel bewerken", <ProfileForm user={data.result} />);
         }
     };
 
-    const { data:workshopData } = useGetAllWorkshopsQuery({ isActive: true });
+    const { data: workshopData } = useGetAllWorkshopsQuery({ isActive: true });
 
     console.log(user);
 
     return (
-        <Grid container spacing={2} border={1} borderColor='#f0f0f0' style={{color: 'black', backgroundColor: '#ffffff', paddingBottom: '15px', paddingRight: '50px'}}>
+        <Grid
+            container
+            spacing={2}
+            border={1}
+            borderColor="#f0f0f0"
+            style={{
+                color: "black",
+                backgroundColor: "#ffffff",
+                paddingBottom: "15px",
+                paddingRight: "50px",
+            }}
+        >
             <FormDialog />
             <Grid item xs={12} md={4}>
                 <Avatar
-                    alt='Profile picture'
+                    alt="Profile picture"
                     src={ProfilePicture}
                     sx={{
                         width: 150,
                         height: 150,
                     }}
                 />
-                <Typography variant='subtitle1' color='secondary'>
+                <Typography
+                    variant="subtitle1"
+                    color="secondary"
+                    paddingTop={"15px"}
+                >
                     Adresgegevens
                 </Typography>
+                <Typography>{user?.location?.address ?? "Onbekend"}</Typography>
+                <Typography>{user?.location?.city ?? "Onbekend"}</Typography>
+                <Typography>{user?.location?.country ?? "Onbekend"}</Typography>
                 <Typography>
-                    {/* {user?.location?.address} */}
-                    adres 12
+                    {user?.location?.postalCode ?? "Onbekend"}
                 </Typography>
-                <Typography>
-                    {/* {user?.location?.city} */}
-                    stad
-                </Typography>
-                <Typography>
-                    {/* {user?.location?.country} */}
-                    land
-                </Typography>
-                <Typography>
-                    {/* {user?.location?.postalCode} */}
-                    1234AB
-                </Typography>
-                <Typography variant='subtitle1' color='secondary'>
+                <Typography
+                    variant="subtitle1"
+                    color="secondary"
+                    paddingTop={"15px"}
+                >
                     Vervoersgegevens
                 </Typography>
-                <Typography>
-                    {`Rijbewijs: ${user?.transport?.hasDriversLicense}`}
-                </Typography>
-                <Typography>
-                    {`Auto: ${user?.transport?.hasVehicle}`}
-                </Typography>
-            </Grid>
-            <Grid item xs={12} md={8}>
-                <Typography fontSize='25px' fontWeight='bold' paddingBottom={'15px'} paddingTop={'15px'}>
-                    {user?.firstName} {user?.lastName}
-                </Typography>
-                <Typography>
-                    {user?.gender}
-                </Typography>
-                <Typography>
-                    {user?.emailAddress}
-                </Typography>
-                <Typography>
-                    {(user?.dateOfBirth != null ? new Date(user?.dateOfBirth).toLocaleDateString('nl-NL') : 'Onbekend')}
-                </Typography>
-                <Typography>
-                    {user?.mobileNumber}
-                </Typography>
-                <Box display='flex' sx={{ justifyContent: 'space-between'}}>
-                    <Typography>
-                        {`Land van herkomst: ${user?.countryOfOrigin}`}
-                    </Typography>
-                    <Typography>
-                        {`Nationaliteit: ${user?.nationality}`}
-                    </Typography>
-                    <Typography>
-                        {`Geboorteplaats: ${user?.placeOfBirth}`}
-                    </Typography>
+                <Box padding={'-100px'}>
+                    <Box display={'flex'} alignItems={'center'}>
+                        <Checkbox checked={user?.transport?.hasDriversLicense ?? false} disabled/>
+                        <Typography>Rijbewijs aanwezig</Typography>
+                    </Box>
+                    <Box display={'flex'} alignItems={'center'}>
+                        <Checkbox checked={user?.transport?.hasVehicle ?? false} disabled/>
+                        <Typography>Auto aanwezig</Typography>
+                    </Box>
                 </Box>
-                <Typography variant='subtitle1' color='secondary'>
+                <Typography
+                    variant="subtitle1"
+                    color="secondary"
+                    paddingTop={"15px"}
+                >
                     Persoonlijke gegevens
                 </Typography>
                 <Typography>
-                    {`KvK: ${user?.kvkNumber}`}
+                    {`KvK: ${user?.kvkNumber ?? "Onbekend"}`}
                 </Typography>
                 <Typography>
-                    {`IBAN: ${user?.paymentInfo?.IBAN}`}
+                    {`IBAN: ${user?.paymentInfo?.IBAN ?? "Onbekend"}`}
                 </Typography>
                 <Typography>
-                    {`BIC: ${user?.paymentInfo?.BIC}`}
+                    {`BIC: ${user?.paymentInfo?.BIC ?? "Onbekend"}`}
                 </Typography>
                 <Typography>
-                    {`Vat ID: ${user?.vatID}`}
+                    {`Vat ID: ${user?.vatID ?? "Onbekend"}`}
                 </Typography>
                 <Typography>
-                    {`Contract: ${user?.contractType}`}
+                    {`Contract: ${user?.contractType ?? "Onbekend"}`}
                 </Typography>
-                <Typography variant='subtitle1' color='secondary'>
+            </Grid>
+            <Grid item xs={12} md={6}>
+                <Typography
+                    fontSize="25px"
+                    fontWeight="bold"
+                    paddingBottom={"15px"}
+                    paddingTop={"15px"}
+                >
+                    {user?.firstName ?? "Onbekend"}{" "}
+                    {user?.lastName ?? "Onbekend"}
+                </Typography>
+                <Typography>
+                    {user?.gender === "f" ? "Vrouw" : "Man"}
+                </Typography>
+                <Typography>{user?.emailAddress ?? "Onbekend"}</Typography>
+                <Typography>
+                    {user?.dateOfBirth != null
+                        ? new Date(user?.dateOfBirth).toLocaleDateString(
+                              "nl-NL"
+                          )
+                        : "Onbekend"}
+                </Typography>
+                <Typography>{user?.mobileNumber}</Typography>
+                <Typography
+                    variant="subtitle1"
+                    color="secondary"
+                    paddingTop={"15px"}
+                >
+                    Oorsprong
+                </Typography>
+                <Typography>
+                    {`Land van herkomst: ${
+                        user?.countryOfOrigin ?? "Onbekend"
+                    }`}
+                </Typography>
+                <Typography>
+                    {`Nationaliteit: ${user?.nationality ?? "Onbekend"}`}
+                </Typography>
+                {/* <Typography>
+                    {`Geboorteplaats: ${user?.placeOfBirth ?? "Onbekend"}`}
+                </Typography> */}
+                <Typography
+                    variant="subtitle1"
+                    color="secondary"
+                    paddingTop={"15px"}
+                >
                     Workshopvoorkeuren
                 </Typography>
 
-                {workshopData?.result && workshopData.result.length > 0 && workshopData.result.filter((workshop) => (
-                    user?.workshopPreferences?.includes(workshop._id)
-                )).map(workshop => (
-                    <div>{workshop.name}</div>
-                ))}
+                {user?.workshopPreferences != null &&
+                    user.workshopPreferences.length > 0 &&
+                    user.workshopPreferences.map((workshop) => (
+                        <Typography>{workshop.name}</Typography>
+                    ))}
 
-                <Typography variant='subtitle1' color='secondary'>
+                <Typography
+                    variant="subtitle1"
+                    color="secondary"
+                    paddingTop={"15px"}
+                >
                     Niveauvoorkeuren
                 </Typography>
 
-                {user?.levelPreferences != null && user?.levelPreferences?.length > 0 && user?.levelPreferences.map((level) => (
-                    <Typography>
-                        {level}
-                    </Typography>
-                ))}
-
+                {user?.levelPreferences != null &&
+                    user?.levelPreferences?.length > 0 &&
+                    user?.levelPreferences.map((level) => (
+                        <Typography>{level}</Typography>
+                    ))}
+            </Grid>
+            <Grid paddingTop={'30px'} xs={12} md={2}>
                 <Button
                     onClick={() => openProfileForm()}
-                    aria-label='edit'
-                    color='primary'>
-                        <Edit />
-                        Profiel bewerken
-                </Button>
-                <Button
-                    // onClick={() => openProfileForm()}
-                    aria-label='addFile'
-                    color='primary'>
-                        <AttachFileIcon />
-                        Voeg een bestand toe
+                    aria-label="edit"
+                    color="primary"
+                >
+                    <Edit />
+                    Profiel bewerken
                 </Button>
             </Grid>
         </Grid>
